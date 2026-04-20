132e6768c21796dda86dc63e08be4c87eb927200
Website Builder <scottbuescher@gmail.com>
2026-04-19 23:05:20 -0400

    fix: remove testimonials section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import { Navbar } from "@/components/Navbar";
 import { Hero } from "@/components/Hero";
 import { FeaturesSection } from "@/components/FeaturesSection";
 import { ScreenshotsSection } from "@/components/ScreenshotsSection";
-import { TestimonialsSection } from "@/components/TestimonialsSection";
 import { PricingSection } from "@/components/PricingSection";
 import { CTASection } from "@/components/CTASection";
 import { Footer } from "@/components/Footer";
@@ -14,7 +13,6 @@ export default function Home() {
       <Hero />
       <ScreenshotsSection />
       <FeaturesSection />
-      <TestimonialsSection />
       <PricingSection />
       <CTASection />
       <Footer />
